6df6d933fca0ee01945af5de40db0f78d65018a6
Carlos Alognon <carlosalognon08@gmail.com>
2024-01-08 20:43:50 +0100

    Add authentication API endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,9 @@
 <?php
 
 use App\Http\Controllers\AppController;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
-use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-// Route::group(
-//     [
-//         'prefix' => LaravelLocalization::setLocale(),
-//         'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
-//     ], function(){
-    Route::get('/', [AppController::class, 'index'])->name('home');
-    Route::get('/about-us', [AppController::class, 'about'])->name('about');
-// });
+
+Route::get('/', [AppController::class, 'index'])->name('home');
+Route::get('/about-us', [AppController::class, 'about'])->name('about');
